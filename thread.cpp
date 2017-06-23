757db9230266fcb3c3e3f24f21aa925c24bc5d69
Venkatesh Macha <linuxvenkey@gmail.com>
2017-06-23 15:44:20 +0530

    cpp multithreading
    Compile using :
     g++ -std=c++11  -Wl,--no-as-needed thread.cpp -lpthread

---
+++ thread.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <thread>
+#define NUM 10
+
+using namespace std;
+
+void * func ( int i) {
+    cout << " Thread Created in Func : " << i <<endl;
+}
+
+int main()
+{
+    //cout << "Hello world!" << endl;
+    thread t[NUM];
+    int i;
+    for (i =0; i<NUM; i++)
+        t[i] = thread(func, i);
+
+    cout << "Lauched from Main "<< endl;
+
+    for(i=0; i<NUM; i++)
+        t[i].join();
+
+    return 0;
+}
